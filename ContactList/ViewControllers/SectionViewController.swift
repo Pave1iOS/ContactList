670bad89aa24f850773130b471831e21010727ae
Pavel Gribachev <PGribachev@yandex.ru>
2023-12-18 22:46:59 +0300

    final settings

--- ContactList/ViewControllers/SectionViewController.swift
+++ ContactList/ViewControllers/SectionViewController.swift
@@ -17,17 +17,38 @@ final class SectionViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
         persons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        persons[section].description
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "sectionCell", for: indexPath)
-        let person = persons[indexPath.row]
+        let person = persons[indexPath.section]
         var content = cell.defaultContentConfiguration()
-        content.text = person.description
+        
+        switch indexPath.row {
+        case 0:
+            content.text = person.phoneNumber
+            content.image = UIImage(systemName: "phone")
+        default:
+            content.text = person.email
+            content.image = UIImage(systemName: "tray")
+        }
+        
         cell.contentConfiguration = content
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
